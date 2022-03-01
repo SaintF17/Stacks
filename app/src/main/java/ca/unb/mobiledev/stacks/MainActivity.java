ee042591a43be3fcd7d527a52a6e0098b17a9608
msotsky <msotsky@mta.ca>
2022-02-28 21:45:47 -0400

    Added splash screen and check boxes.

--- app/src/main/java/ca/unb/mobiledev/stacks/MainActivity.java
+++ app/src/main/java/ca/unb/mobiledev/stacks/MainActivity.java
@@ -2,13 +2,26 @@ package ca.unb.mobiledev.stacks;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+    private final long SPLASH_TIME = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,HomeActivity.class);
+                MainActivity.this.startActivity(intent);
+                MainActivity.this.finish();
+            }
+        }, SPLASH_TIME);
     }
 }

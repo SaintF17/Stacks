86dd056f18f8bc8feb99cbda04a0bcc4ec214085
Saint17 <56317638+Saint17@users.noreply.github.com>
2022-03-01 00:36:15 -0400

    Navigation page template added

--- app/src/main/java/ca/unb/mobiledev/stacks/HomeActivity.java
+++ app/src/main/java/ca/unb/mobiledev/stacks/HomeActivity.java
@@ -2,7 +2,10 @@ package ca.unb.mobiledev.stacks;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button next = findViewById(R.id.catergory_next_btn);
+        next.setOnClickListener(view -> {
+            Intent intent = new Intent(HomeActivity.this, NavigationActivity.class);
+            startActivity(intent);
+        });
     }
 }

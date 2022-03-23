c67c069c50e624b0b9dbc9de4e4358d1af6d0b5e
Saint17 <56317638+Saint17@users.noreply.github.com>
2022-03-22 22:49:29 -0300

    Changed item array to arraylist.

--- app/src/main/java/ca/unb/mobiledev/stacks/utils/CategoryObject.java
+++ app/src/main/java/ca/unb/mobiledev/stacks/utils/CategoryObject.java
@@ -2,17 +2,20 @@ package ca.unb.mobiledev.stacks.utils;
 
 import android.widget.EditText;
 
+import java.util.ArrayList;
+
 public class CategoryObject {
     private String name;
     private EditText budgetText;
 
     // Store the related items in a category in an arr of ItemObjects
     // would this be easier if implemented as a <T> instead (or linked list?)
-    private ItemObject[] itemArr = new ItemObject[0];
+    private ArrayList<ItemObject> itemObjects;
 
     public CategoryObject(String name, EditText text) {
         this.name = name;
         this.budgetText = text;
+        itemObjects = new ArrayList<>();
     }
 
     public String getName() {
